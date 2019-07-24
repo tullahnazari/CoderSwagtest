4856b6998e92177d7ec86dd8226f91017d8a2eea
Mr. Tee <tullah.nazari@gmail.com>
2019-07-24 13:39:36 -0500

    created listview, added adapter

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.coderswag.Model.Category
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //getting data into UI element (list view). You need an adapter for that (needs to know context, the layout, and where the hell the data is) :)
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, DataService.categories)
+        categoryListView.adapter = adapter
     }
+
 }
